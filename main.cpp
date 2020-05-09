4e6eeeea6ed54708a6c761c9a81e37871a18cdac
Stepan <Goryachev.stepan@yandex.ru>
2020-05-09 16:42:56 +0700

    Wrote a function that adds another word to the word list

--- main.cpp
+++ main.cpp
@@ -25,6 +25,53 @@ bool try_read_data(string& bufer, string file_name)
 	return true;
 }
 
+void add_to_list(char* word, list_element& head, list_element& tail)
+{
+	if (head.data == NULL)
+	{
+		list_element* elem = new list_element(word);
+		head = *elem;
+		tail = head;
+	}
+	else
+	{
+		list_element* new_elem = new list_element(word);
+		list_element* elem = &head;
+		while (compare(elem->data, word) == -1)
+		{
+			if (elem->next == NULL)
+				break;
+			elem = elem->next;
+		}
+
+		if (elem == &head && compare(elem->data, word) >= 0)
+		{
+			new_elem->next = elem->next;
+			elem->next = new_elem;
+			if (new_elem->next != NULL)
+				new_elem->next->prev = new_elem;
+			new_elem->prev = elem;
+			new_elem->data = elem->data;
+			elem->data = word;
+		}
+		else if (elem->next == NULL && compare(elem->data, word) == -1)
+		{
+
+			new_elem->prev = elem;
+			new_elem->next = NULL;
+			elem->next = new_elem;
+			tail = *new_elem;
+		}
+		else
+		{
+			elem->prev->next = new_elem;
+			new_elem->prev = elem->prev;
+			new_elem->next = elem;
+			elem->prev = new_elem;
+		}
+	}
+}
+
 void parse(string& bufer, list_element& head, list_element& tail)
 {
 	bool new_word = true;
